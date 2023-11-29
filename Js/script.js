5b1c0a726fa40101a21db0ca82c3137e5ff81bf8
Yashika Tanwar <yashika.tanwar567@gmail.com>
2023-11-29 22:58:12 +0530

    MajorProject1

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,7 @@
 const pickUser=(hand)=>{
 console.log(hand);
+//after selecting the options we want the user to move to the next page
+// but it should hide the current page as well
+let content=document.querySelector(".content");
+content.style.display="none";
 } 
