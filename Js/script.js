11ae4782e710be0f80bd16efdf859bfda819997b
Yashika Tanwar <yashika.tanwar567@gmail.com>
2023-11-30 22:20:31 +0530

    MajorProject1

--- Js/script.js
+++ Js/script.js
@@ -4,11 +4,11 @@ const userOptions={
     "paper":"/assets/Paper.png",
     "scissor":"/assets/Scissors.png"
 };
-
+let UserScore=0;
+let PcScore=0;
 /*arrow function for user values*/
 
 const pickUser=(hand)=>{
-    console.log(hand);
 
     //after selecting the options we want the user to move to the next page but it should hide the current page as well
     
@@ -23,4 +23,75 @@ const pickUser=(hand)=>{
 
     //set user pick using dictionary
     document.querySelector("#option_user").src=userOptions[hand];
+    pickComputer(hand); // we have saved the value of pc option here
+    
+}
+
+const pickComputer=(hand)=>{
+    let hands=["stone","paper","scissor"];
+    let pcHands=hands[Math.floor(Math.random()*3)];
+    console.log(pcHands);
+    document.querySelector("#option_pc").src=userOptions[pcHands];
+    // return pcHands;
+    referee(hand,pcHands); 
+}
+
+// to calculate score
+const referee=(userHand,pcHands)=>{
+    if(userHand=="paper" && pcHands=="scissor")
+    {
+        Decision("YOU LOSE");
+        SetpcScore(PcScore + 1);
+    }
+    if(userHand=="paper" && pcHands=="rock")
+    {
+        Decision("YOU WIN");
+        SetUserScore(UserScore + 1);
+    }
+    if(userHand=="paper" && pcHands=="paper")
+    {
+        Decision("IT's A TIE");
+    }
+    if(userHand=="rock" && pcHands=="scissor")
+    {
+        Decision("YOU WIN");
+        SetUserScore(UserScore + 1);
+    }
+    if(userHand=="rock" && pcHands=="paper")
+    {
+        Decision("YOU LOSE");
+        SetpcScore(PcScore + 1);
+    }
+    if(userHand=="rock" && pcHands=="rock")
+    {
+        Decision("IT's A TIE");
+    }
+    if(userHand=="scissor" && pcHands=="paper")
+    {
+        Decision("YOU WIN");
+        SetUserScore(UserScore + 1);
+    }
+    if(userHand=="scissor" && pcHands=="rock")
+    {
+        Decision("YOU LOSE");
+        SetpcScore(PcScore + 1);
+    }
+    if(userHand=="scissor" && pcHands=="scissor")
+    {
+        Decision("IT's A TIE");
+    }
+}
+
+const Decision=(decision)=>{
+    console.log(decision);
+    document.querySelector("#rank").innerHTML=decision;
+}
+
+const SetUserScore=(score)=>{
+    // console.log(score);
+    document.querySelector("#your_score").innerHTML=score;
+}
+
+const SetpcScore=(score)=>{
+    document.querySelector("#computer_score").innerHTML=score;
 }
