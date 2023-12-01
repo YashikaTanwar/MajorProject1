5d08525aa39583d6910a3f798661c878e3d65623
Yashika Tanwar <yashika.tanwar567@gmail.com>
2023-12-01 22:49:04 +0530

    MajorProject1

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,6 @@
 /*creating a dictionary */
 const userOptions={
-    "stone":"/assets/Rock.png",
+    "rock":"/assets/Rock.png",
     "paper":"/assets/Paper.png",
     "scissor":"/assets/Scissors.png"
 };
@@ -18,73 +18,79 @@ const pickUser=(hand)=>{
     let contest=document.querySelector(".contest");
     contest.style.display="flex";
 
-    let next=document.querySelector(".rules");
-    next.style.display="flex";
-
     //set user pick using dictionary
     document.querySelector("#option_user").src=userOptions[hand];
     pickComputer(hand); // we have saved the value of pc option here
     
 }
 
+const btnrules=document.querySelector("#rules-btn");
+const rules=document.querySelector("#rules");
+const rulesimg=document.querySelector(".modal_container img");
+const modal=document.querySelector(".modal");
+
+btnrules.addEventListener('click',()=>{
+    modal.classList.toggle('show-modal');
+})
+rules.addEventListener('click',()=>{
+    modal.classList.toggle('show-modal');
+})
+rulesimg.addEventListener('click',()=>{
+    modal.classList.toggle('show-modal');
+})
+
 const pickComputer=(hand)=>{
-    let hands=["stone","paper","scissor"];
-    let pcHands=hands[Math.floor(Math.random()*3)];
-    console.log(pcHands);
+    let hands=["rock","paper","scissor"];
+    let pcHands=hands[Math.floor(Math.random()*hands.length)];
+    
     document.querySelector("#option_pc").src=userOptions[pcHands];
     // return pcHands;
     referee(hand,pcHands); 
 }
 
 // to calculate score
-const referee=(userHand,pcHands)=>{
-    if(userHand=="paper" && pcHands=="scissor")
-    {
+const referee = (userHand, pcHands) => {
+    if (userHand === "paper" && pcHands === "scissor") {
         Decision("YOU LOSE");
         SetPCScore(PcScore + 1);
-    }
-    if(userHand=="paper" && pcHands=="rock")
-    {
+    } else if (userHand === "paper" && pcHands === "rock") {
         Decision("YOU WIN");
         SetUSERScore(UserScore + 1);
-    }
-    if(userHand=="paper" && pcHands=="paper")
-    {
-        Decision("IT's A TIE");
-    }
-    if(userHand=="rock" && pcHands=="scissor")
-    {
+    } else if (userHand === "rock" && pcHands === "scissor") {
         Decision("YOU WIN");
         SetUSERScore(UserScore + 1);
-    }
-    if(userHand=="rock" && pcHands=="paper")
-    {
+    } else if (userHand === "rock" && pcHands === "paper") {
         Decision("YOU LOSE");
         SetPCScore(PcScore + 1);
-    }
-    if(userHand=="rock" && pcHands=="rock")
-    {
-        Decision("IT's A TIE");
-    }
-    if(userHand=="scissor" && pcHands=="paper")
-    {
+    } else if (userHand === "scissor" && pcHands === "paper") {
         Decision("YOU WIN");
         SetUSERScore(UserScore + 1);
-    }
-    if(userHand=="scissor" && pcHands=="rock")
-    {
+    } else if (userHand === "scissor" && pcHands === "rock") {
         Decision("YOU LOSE");
         SetPCScore(PcScore + 1);
-    }
-    if(userHand=="scissor" && pcHands=="scissor")
-    {
+    } else if (userHand === pcHands) {
         Decision("IT's A TIE");
     }
-}
+};
 
 const Decision=(decision)=>{
-    console.log(decision);
     document.querySelector("#rank").innerHTML=decision;
+        
+    if(decision=="YOU WIN"){
+        let rules=document.querySelector(".rules");
+        rules.style.display="flex";
+
+        let next=document.querySelector(".next");
+        next.style.display="flex";
+    }
+    else{
+        let rules=document.querySelector(".rules");
+        rules.style.display="flex";
+
+        let next=document.querySelector(".next");
+        next.style.display="none";
+    }
+    
 }
 
 const SetUSERScore=(userscore)=>{
